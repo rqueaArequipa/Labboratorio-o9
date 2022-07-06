0e35be1a0d2f3eb3b7385495d28a97660f25f66a
rqueatecsuparequipa <rqueatecsuparequipa>
2022-07-06 15:05:13 -0500

    creacion de tablas y migraciones

---
+++ database/migrations/2022_07_06_193200_create_carrito_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarritoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carrito', function (Blueprint $table) {
+            $table->id();
+            $table->string('articulo_id');
+            $table->string('usuario_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carrito');
+    }
+}
